948de786aac791457e3cd634c2a2e3f73a418b10
unknown <zylegaspi@gmail.com>
2018-12-06 03:25:38 +0800

    add feature image upload

--- db_trace.sql
+++ db_trace.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 29, 2018 at 05:59 AM
+-- Generation Time: Dec 05, 2018 at 04:47 PM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.11
 
@@ -40,7 +40,10 @@ CREATE TABLE `contacts` (
 --
 
 INSERT INTO `contacts` (`contact_id`, `user_id`, `friend_id`, `date_added`) VALUES
-(13, 13, 14, '2018-11-28 16:53:50');
+(22, 15, 12, '2018-11-30 07:16:28'),
+(29, 12, 15, '2018-11-30 09:27:33'),
+(30, 12, 12, '2018-11-30 09:36:20'),
+(31, 12, 13, '2018-11-30 10:11:24');
 
 -- --------------------------------------------------------
 
@@ -59,7 +62,28 @@ CREATE TABLE `notification_type` (
 
 INSERT INTO `notification_type` (`notification_type_id`, `type`) VALUES
 (1, 'Contact Notification'),
-(2, 'Trace Request');
+(2, 'Trace Request'),
+(3, 'Group Notification');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trace_group`
+--
+
+CREATE TABLE `trace_group` (
+  `group_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `trace_group`
+--
+
+INSERT INTO `trace_group` (`group_id`, `name`, `date_created`) VALUES
+(3, 'sample group', '2018-12-02 06:45:20'),
+(4, 'group2', '2018-12-02 06:50:08');
 
 -- --------------------------------------------------------
 
@@ -71,7 +95,6 @@ CREATE TABLE `user` (
   `user_id` int(11) NOT NULL,
   `firstname` varchar(255) NOT NULL,
   `lastname` varchar(255) NOT NULL,
-  `middlename` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `isPremium` tinyint(4) NOT NULL,
   `date_created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
@@ -82,10 +105,32 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`user_id`, `firstname`, `lastname`, `middlename`, `password`, `isPremium`, `date_created`, `email`) VALUES
-(12, 'Ver', 'legaspi', 'Ver', '$2b$10$2Q4duKmUfRUJn50fkwMaGeE2mquoyM2veyrV8BFOHCng8JdsJUM9i', 0, '2018-11-25 11:25:57', 'zylegaspi@gmail.com'),
-(13, 'f', 'f', 'f', '$2b$10$zQDL5bEdbmXqlZ/pm8G7au4Tos7VuOP9lHlSsGN155655wg8Aq.U2', 0, '2018-11-25 11:35:11', 'f'),
-(14, 'first', 'last', 'first', '$2b$10$OpcSu7dFXpxfJ80/s1zfWu6KoiP3H37Vt8KDEQcDtceTk10PxgG42', 0, '2018-11-28 15:26:29', 'g');
+INSERT INTO `user` (`user_id`, `firstname`, `lastname`, `password`, `isPremium`, `date_created`, `email`) VALUES
+(12, 'Ver', 'legaspi', '$2b$10$2Q4duKmUfRUJn50fkwMaGeE2mquoyM2veyrV8BFOHCng8JdsJUM9i', 0, '2018-11-25 11:25:57', 'zylegaspi@gmail.com'),
+(13, 'f', 'f', '$2b$10$zQDL5bEdbmXqlZ/pm8G7au4Tos7VuOP9lHlSsGN155655wg8Aq.U2', 0, '2018-11-25 11:35:11', 'f'),
+(14, 'first', 'last', '$2b$10$OpcSu7dFXpxfJ80/s1zfWu6KoiP3H37Vt8KDEQcDtceTk10PxgG42', 0, '2018-11-28 15:26:29', 'g'),
+(15, 'shane', 'royena', '$2b$10$Z7IMhEmTYNuCxfYEBpl5NOjPAiL8w5qanBS0Ma5/JizfcpHgCH3x6', 0, '2018-11-30 06:48:17', 'royenashane@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_group`
+--
+
+CREATE TABLE `user_group` (
+  `user_group_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  `isAdmin` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_group`
+--
+
+INSERT INTO `user_group` (`user_group_id`, `user_id`, `group_id`, `isAdmin`) VALUES
+(2, 12, 3, 1),
+(3, 12, 4, 1);
 
 -- --------------------------------------------------------
 
@@ -123,10 +168,24 @@ ALTER TABLE `notification_type`
   ADD PRIMARY KEY (`notification_type_id`);
 
 --
+-- Indexes for table `trace_group`
+--
+ALTER TABLE `trace_group`
+  ADD PRIMARY KEY (`group_id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `user_group`
+--
+ALTER TABLE `user_group`
+  ADD PRIMARY KEY (`user_group_id`),
+  ADD KEY `group_id` (`group_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `user_notification`
@@ -145,25 +204,37 @@ ALTER TABLE `user_notification`
 -- AUTO_INCREMENT for table `contacts`
 --
 ALTER TABLE `contacts`
-  MODIFY `contact_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `contact_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT for table `notification_type`
 --
 ALTER TABLE `notification_type`
-  MODIFY `notification_type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `notification_type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `trace_group`
+--
+ALTER TABLE `trace_group`
+  MODIFY `group_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `user_group`
+--
+ALTER TABLE `user_group`
+  MODIFY `user_group_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `user_notification`
 --
 ALTER TABLE `user_notification`
-  MODIFY `user_notification_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `user_notification_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -175,6 +246,13 @@ ALTER TABLE `user_notification`
 ALTER TABLE `contacts`
   ADD CONSTRAINT `contacts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `contacts_ibfk_2` FOREIGN KEY (`friend_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `user_group`
+--
+ALTER TABLE `user_group`
+  ADD CONSTRAINT `user_group_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `trace_group` (`group_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_group_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `user_notification`
--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const secretKey = "traceme";
 const cors = require('cors');
+const formidable = require('formidable');
+var uniqid = require('uniqid');
 
 app.use(bodyParser.json());
 app.use(cors({credentials: true}));
@@ -71,24 +73,57 @@ const conn = mysql.createConnection({
 //Connect to database
 conn.connect();
 
-app.post('/sign-up', (req,res)=>{
+function uploadPhoto(req,res,next){
+    var form = new formidable.IncomingForm();
+    var profilePath;
 
-    let hash = bcrypt.hashSync(req.body.user.password, 10);
+    form.parse(req, (err,fields,files)=>{
+        req.body = fields;
+    });
+
+    form.on('fileBegin', function (name, file){
+        let newImageName = uniqid()+'.'+ file.name.split('.').pop();
+        profilePath = __dirname + '/images/' + newImageName;
+        file.path = profilePath;
+        profilePath = 'http://localhost:8080/images/' + newImageName;
+    });
+
+    form.on('file', function (name, file){
+        req.body.profile_picture = profilePath;
+        console.log('Uploaded ' + file.name);
+    });
+
+    form.on('end', () => {
+        req.body.profile_picture = profilePath;
+        next();
+    });
+
+}
+
+app.post('/sign-up',uploadPhoto, (req,res) => {
+
+    console.log(req.body);
+
+    let hash = bcrypt.hashSync(req.body.password, 10);
 
     let sql = ` INSERT INTO user SET
-    firstname = '${req.body.user.firstname}',
-    lastname = '${req.body.user.lastname}',
+    firstname = '${req.body.firstname}',
+    lastname = '${req.body.lastname}',
     password = '${hash}',
-    email = '${req.body.user.email}'`;
+    email = '${req.body.email}'`;
 
+    if(!!req.body.profile_picture) {
+        sql += `, profile_picture ='${req.body.profile_picture}'`;
+    }
+
+    console.log(conn.query(sql).sql);
     conn.query(sql, (err,result)=> {
         if(err) throw err;
-        res.setHeader('Content-type','Application/json');
         let user = {
             user_id : result.insertId,
             email: req.body.email,
-            firstname: req.body.user.firstname,
-            lastname: req.body.user.lastname,
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
             isPremium: false
         };
         const token = jwt.sign({user},secretKey);
@@ -116,7 +151,7 @@ function verifyToken(req,res,next){
 }
 
 app.post('/login',(req,res)=>{
-    res.setHeader('Content-type','Application/json');
+
     let sql = `SELECT * FROM user WHERE email = '${req.body.email}'`;
     conn.query(sql, (err,result)=>{
         if (err) throw err;
@@ -293,3 +328,6 @@ app.get('/group',verifyToken, (req,res)=>{
         res.json(result);
     });
 });
+
+//serve images
+app.use('/images',express.static('images'));
