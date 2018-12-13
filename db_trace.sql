ef37545eefaa3604bd2a2676f17970e36d247f0f
unknown <zylegaspi@gmail.com>
2018-12-13 13:12:10 +0800

    added database

--- db_trace.sql
+++ db_trace.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 05, 2018 at 04:47 PM
+-- Generation Time: Dec 13, 2018 at 06:10 AM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.11
 
@@ -40,10 +40,7 @@ CREATE TABLE `contacts` (
 --
 
 INSERT INTO `contacts` (`contact_id`, `user_id`, `friend_id`, `date_added`) VALUES
-(22, 15, 12, '2018-11-30 07:16:28'),
-(29, 12, 15, '2018-11-30 09:27:33'),
-(30, 12, 12, '2018-11-30 09:36:20'),
-(31, 12, 13, '2018-11-30 10:11:24');
+(1, 23, 22, '2018-12-13 02:24:21');
 
 -- --------------------------------------------------------
 
@@ -98,18 +95,17 @@ CREATE TABLE `user` (
   `password` varchar(255) NOT NULL,
   `isPremium` tinyint(4) NOT NULL,
   `date_created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `email` varchar(255) NOT NULL
+  `email` varchar(255) NOT NULL,
+  `profile_picture` varchar(255) NOT NULL DEFAULT 'http://localhost:8080/images/default.png'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`user_id`, `firstname`, `lastname`, `password`, `isPremium`, `date_created`, `email`) VALUES
-(12, 'Ver', 'legaspi', '$2b$10$2Q4duKmUfRUJn50fkwMaGeE2mquoyM2veyrV8BFOHCng8JdsJUM9i', 0, '2018-11-25 11:25:57', 'zylegaspi@gmail.com'),
-(13, 'f', 'f', '$2b$10$zQDL5bEdbmXqlZ/pm8G7au4Tos7VuOP9lHlSsGN155655wg8Aq.U2', 0, '2018-11-25 11:35:11', 'f'),
-(14, 'first', 'last', '$2b$10$OpcSu7dFXpxfJ80/s1zfWu6KoiP3H37Vt8KDEQcDtceTk10PxgG42', 0, '2018-11-28 15:26:29', 'g'),
-(15, 'shane', 'royena', '$2b$10$Z7IMhEmTYNuCxfYEBpl5NOjPAiL8w5qanBS0Ma5/JizfcpHgCH3x6', 0, '2018-11-30 06:48:17', 'royenashane@gmail.com');
+INSERT INTO `user` (`user_id`, `firstname`, `lastname`, `password`, `isPremium`, `date_created`, `email`, `profile_picture`) VALUES
+(22, 'f', 'f', '$2b$10$7VPZInatXRuYTNyf7XH8Vuh865qqKJw6/6Y2Jzgomrysc2esmQeFW', 0, '2018-12-05 19:54:02', 'f', 'http://localhost:8080/images/default.png'),
+(23, 'g', 'g', '$2b$10$a5SBq8Ppu5sdWc7go1GeV.TmNuJ6W1Lt3IMAO06MS7aVmwj3.Bmfi', 0, '2018-12-05 19:54:29', 'g', 'http://localhost:8080/images/nonn1b4jpblcpy2.jpg');
 
 -- --------------------------------------------------------
 
@@ -123,14 +119,6 @@ CREATE TABLE `user_group` (
   `group_id` int(11) NOT NULL,
   `isAdmin` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `user_group`
---
-
-INSERT INTO `user_group` (`user_group_id`, `user_id`, `group_id`, `isAdmin`) VALUES
-(2, 12, 3, 1),
-(3, 12, 4, 1);
 
 -- --------------------------------------------------------
 
@@ -204,7 +192,7 @@ ALTER TABLE `user_notification`
 -- AUTO_INCREMENT for table `contacts`
 --
 ALTER TABLE `contacts`
-  MODIFY `contact_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `contact_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `notification_type`
@@ -222,19 +210,19 @@ ALTER TABLE `trace_group`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `user_group`
 --
 ALTER TABLE `user_group`
-  MODIFY `user_group_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `user_group_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user_notification`
 --
 ALTER TABLE `user_notification`
-  MODIFY `user_notification_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_notification_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
